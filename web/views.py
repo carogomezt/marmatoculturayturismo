681edbf8b82bceb924a405bc9d5863c80fca5ac7
carogomezt <carolina9511@gmail.com>
2018-08-29 23:19:19 -0500

    Adding Marmato researchs

--- web/views.py
+++ web/views.py
@@ -7,4 +7,15 @@ def home():
     with open('data/galeria.json', encoding='utf8') as data_file:    
         galeria = json.load(data_file)
     
-    return render_template('index.html', galeria=galeria)
+    with open('data/investigaciones.json', encoding='utf8') as data_file:    
+        investigaciones = json.load(data_file)
+    
+    return render_template('index.html', galeria=galeria, investigaciones=investigaciones[:(4 - len(investigaciones))])
+
+@app.route('/investigaciones/')
+def investigaciones():
+    
+    with open('data/investigaciones.json', encoding='utf8') as data_file:    
+        investigaciones = json.load(data_file)
+    
+    return render_template('investigaciones.html', investigaciones=investigaciones)
