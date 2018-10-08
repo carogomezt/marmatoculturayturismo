516fdfaafd3c5c9760cf68da7e397177c413a4e2
carogomezt <carolina9511@gmail.com>
2018-10-07 22:14:03 -0500

    Adding legal documents

--- web/views.py
+++ web/views.py
@@ -1,13 +1,14 @@
 from flask import render_template
 from app import app, pages
 import json
+import io
 
 @app.route('/')
 def home():
-    with open('data/galeria.json', encoding='utf8') as data_file:    
+    with io.open('data/galeria.json', encoding='utf8') as data_file:    
         galeria = json.load(data_file)
     
-    with open('data/investigaciones.json', encoding='utf8') as data_file:    
+    with io.open('data/investigaciones.json', encoding='utf8') as data_file:    
         investigaciones = json.load(data_file)
     
     return render_template('index.html', galeria=galeria, investigaciones=investigaciones[:(4 - len(investigaciones))])
@@ -15,7 +16,15 @@ def home():
 @app.route('/investigaciones/')
 def investigaciones():
     
-    with open('data/investigaciones.json', encoding='utf8') as data_file:    
+    with io.open('data/investigaciones.json', encoding='utf8') as data_file:    
         investigaciones = json.load(data_file)
     
     return render_template('investigaciones.html', investigaciones=investigaciones)
+
+@app.route('/documentoslegales/')
+def documentoslegales():
+    
+    with io.open('data/documentoslegales.json', encoding='utf8') as data_file:    
+        documentoslegales = json.load(data_file)
+    
+    return render_template('documentoslegales.html', documentoslegales=documentoslegales)
